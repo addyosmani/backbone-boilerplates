b4c0e76298ae4d41ec9bbba5012ef63bb5889757
addyosmani <addyosmani@gmail.com>
2011-12-25 19:21:23 +0000

    minor updates to avooid jade parsing

--- option1/app.js
+++ option1/app.js
@@ -20,8 +20,8 @@ app.configure(function(){
   app.use(app.router);
   app.use(express.static(path.join(application_root, "public")));
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-  app.set('views', path.join(application_root, "views"));
-  app.set('view engine', 'jade')
+  //app.set('views', path.join(application_root, "views"));
+  //app.set('view engine', 'jade')
 });
 
 app.get('/', function(req, res){
